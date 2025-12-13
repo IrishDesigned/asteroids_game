ed0f231bcce83ae6b3480c80ea13e9720b21745c
Michael <Michael@IrishDesigned>
2025-12-13 15:55:41 +0000

    Updated logic to detect when window is closed with button

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
 from logger import log_state
 
-def game_loop():
+def game_loop(screen):
     run_game_loop = True
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
     while run_game_loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         log_state()
         screen.fill((0,0,0))
         pygame.display.flip()
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"""Screen width: {SCREEN_WIDTH}
+    print(f"""
+    Screen width: {SCREEN_WIDTH}
     Screen height: {SCREEN_HEIGHT}""")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_loop()
+    game_loop(screen)
     
 
 if __name__ == "__main__":
