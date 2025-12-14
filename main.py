758daa248c5629583ac22a71f8f2836a56195e6d
Michael | IrishDesigned <michael@irishdesigned.com>
2025-12-14 00:16:23 +0000

    make the game_loop() refresh at 60 fps

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
 from logger import log_state
 
-def game_loop(screen):
+def game_loop(screen, clock):
     run_game_loop = True
+    dt = 0
     while run_game_loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -12,15 +13,19 @@ def game_loop(screen):
         log_state()
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        # print(dt) # debug
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"""
-    Screen width: {SCREEN_WIDTH}
-    Screen height: {SCREEN_HEIGHT}""")
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}
+""")
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_loop(screen)
+    game_loop(screen, clock)
     
 
 if __name__ == "__main__":
